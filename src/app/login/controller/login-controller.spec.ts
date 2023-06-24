c538c55d5337d8746b2cbf9f2f322f962991d598
andrezanlorensi <andrezanlorensi@hotmail.com>
2023-06-24 16:20:00 -0300

    primeiro

---
+++ src/app/login/controller/login-controller.spec.ts
@@ -0,0 +1,7 @@
+import { LoginController } from './login-controller';
+
+describe('LoginController', () => {
+  it('should create an instance', () => {
+    expect(new LoginController()).toBeTruthy();
+  });
+});
